dfd5664759dd3c9e25132b5c0da1293cd117608a
luckoff <neluckoff@gmail.com>
2022-08-12 18:00:06 +0300

    final commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.1'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='whats_api',
+    version=version,
+
+    author='neluckoff',
+    author_email='neluckoff@gmail.com',
+
+    description=(
+        u'Open source module for sending messages to WhatsApp based on Selenium'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/neluckoff/whats_api/archive/main.zip',
+    download_url='https://github.com/neluckoff/whats_api/archive/master.zip',
+
+    license='MIT License, see LICENSE file',
+
+    packages=['whats_api'],
+    install_requires=['selenium', 'qrcode', 'Pillow'],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
--- whats_api/__init__.py
+++ whats_api/__init__.py
@@ -1,2 +1,13 @@
+"""
+:authors: neluckoff
+:email: neluckoff@gmail.com
+:copyright: (c) 2022 neluckoff
+"""
+
 from .user import User
 from .client import Client
+
+
+__author__ = 'neluckoff'
+__version__ = '0.0.1'
+__email__ = 'neluckoff@gmail.com'
